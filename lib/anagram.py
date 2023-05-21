d13fc8ca081915f31202b73e8865e6bbd2700c51
Sofia Katzman <katzmansof@gmail.com>
2023-05-21 00:58:11 -0400

    finished lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+        sorted_word = sorted(self.word.lower())
+        
+        for word in words:
+            # Ignore identical words
+            if word.lower() == self.word.lower():
+                continue
+
+            # Check if the sorted characters of the word match the sorted characters of self.word
+            if sorted(word.lower()) == sorted_word:
+                anagrams.append(word)
+
+        return anagrams
